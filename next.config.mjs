c1ad93ec842fcdfb867e6b1f51cb65c529939be8
MungangaThelly <daddymulongo@yahoo.fr>
2026-02-11 05:48:23 +0100

    Remove www redirect to test base domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.nuhar.se',
-          },
-        ],
-        destination: 'https://nuhar.se/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
